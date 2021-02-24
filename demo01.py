7dddd4c535e37d1386e18e0ed545cfb775b89837
lichunlin <1486505344@qq.com>
2021-02-24 15:29:10 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,41 @@
+'''print("Hello,World!")#字符串
+print(2333)#整数
+print(2.333)#小数
+print(True)#布尔值  Ture  False
+print(())#元组
+print([])#数组
+print({})#字典
+print("哈哈",2333,2.333)
+print("哈哈"+"嘻嘻")#字符串的拼接
+print("哈哈"*100)
+print(((1+2)*100-99)/2)
+print(2>3)
+
+变量
+赋值
+name = "张三"#把张三这个值赋值给了名字叫a的这个变量
+print(name)'''
+'''a = input("请输入：")
+b = input("请输入：")
+print("input获取的内容：",a+b)'''
+
+#数据格式的转换
+'''print(type("哈哈"))#字符串  str
+print(type(2333))#整数  int
+print(type(2.333))#小数  float
+print(type(()))#元组  tuple
+print(type([]))#数组 list
+print(type({}))#字典 dict'''
+
+# a = float(input("请输入："))
+# b = float(input("请输入："))
+# print("input获取的内容：",a+b)
+
+# a = '123456789123456789123456789'
+# print(len(a))
+
+
+#通过代码获取两段内容，并且计算他们的长度的和。
+a = input("请输入：")
+b = input("请输入：")
+print("他们的长度为：",len(a)+len(b))
